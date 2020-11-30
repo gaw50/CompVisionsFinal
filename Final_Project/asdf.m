a6655225e2de4977a7c50ee8aebae7386938b4b8
rbg39 <55247154+rbg39@users.noreply.github.com>
2020-11-30 14:36:58 -0600

    Update asdf.m

--- Final_Project/asdf.m
+++ Final_Project/asdf.m
@@ -9,29 +9,7 @@ addpath C:\Users\Reagan\Desktop\test_face_photos
 % Get list of all BMP files in this directory
 % DIR returns as a structure array.  You will need to use () and . to get
 % the file names.
-cd C:\Users\Reagan\Desktop\test_face_photos
-imagefiles = dir();      
-nfiles = length(imagefiles); %Number of files found
-nfiles = nfiles-1;
-currentfilename = imagefiles(3).name;
-currentimage = imread(currentfilename);
-count = 0
-for i=3:nfiles
-   currentfilename = imagefiles(i).name;
-   currentimage = imread(currentfilename);
-   
-   x = size(currentimage, 2);
-   y = size(currentimage, 1);
-   
-   testFacePhotos{i} = currentimage;
-   count = count + 1;
-   x = 4;
-end
-
-save testFacePhotos testFacePhotos
-
-%cd C:\Users\Reagan\Desktop\training_nonfaces
-trainingNonFaces = zeros(x, y, nfiles);
+cd C:\Users\Reagan\Desktop\training_faces
 imagefiles = dir();      
 nfiles = length(imagefiles); %Number of files found
 nfiles = nfiles-1;
@@ -40,18 +18,42 @@ currentimage = imread(currentfilename);
 x = size(currentimage, 1);
 y = size(currentimage, 2);
 trainingFaces = zeros(x, y, nfiles);
+count = 0
+for i=3:nfiles
+   currentfilename = imagefiles(i).name;
+   currentimage = imread(currentfilename);
+   
+   x = size(currentimage, 2);
+   y = size(currentimage, 1);
+   
+   trainingFaces(:,:,i) = currentimage;
+   count = count + 1;
+   x = 4;
+end
+
+save trainingFaces trainingFaces
+
+cd C:\Users\Reagan\Desktop\training_nonfaces
+trainingNonFaces = zeros(x, y, nfiles);
+imagefiles = dir();      
+nfiles = length(imagefiles); %Number of files found
+nfiles = nfiles-1;
+currentfilename = imagefiles(3).name;
+currentimage = imread(currentfilename);
+x = size(currentimage, 1);
+y = size(currentimage, 2);
 trainingNonFaces = zeros(x, y, nfiles);
 for i=3:nfiles
    currentfilename = imagefiles(i).name;
    currentimage = imread(currentfilename);
    
-   trainingNonFaces{i} = currentimage;
+   trainingNonFaces(:,:,i) = currentimage;
    
    x = 4;
 end
 
 
-%save trainingNonFaces
+save trainingNonFaces
 
 
 cd C:\Users\Reagan\Desktop\test_cropped_faces
